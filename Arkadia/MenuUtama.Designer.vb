7154d8aa210dc841cb549cfb1d9bf8837e9f5e8a
manchoydotid <nurmanchoy@gmail.com>
2019-07-01 13:37:16 +0700

    add menu

---
+++ Arkadia/MenuUtama.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MenuUtama
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.AncamanBom = New System.Windows.Forms.PictureBox()
+        Me.GempaBumi = New System.Windows.Forms.PictureBox()
+        Me.Kebakaran = New System.Windows.Forms.PictureBox()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.AncamanBom, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GempaBumi, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Kebakaran, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackgroundImage = Global.Arkadia.My.Resources.Resources.Menu_background_paging_system
+        Me.Panel1.Controls.Add(Me.PictureBox5)
+        Me.Panel1.Controls.Add(Me.PictureBox4)
+        Me.Panel1.Controls.Add(Me.PictureBox3)
+        Me.Panel1.Controls.Add(Me.AncamanBom)
+        Me.Panel1.Controls.Add(Me.GempaBumi)
+        Me.Panel1.Controls.Add(Me.Kebakaran)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1016, 741)
+        Me.Panel1.TabIndex = 0
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.Image = Global.Arkadia.My.Resources.Resources.pengetesan
+        Me.PictureBox5.Location = New System.Drawing.Point(805, 575)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(161, 53)
+        Me.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.PictureBox5.TabIndex = 5
+        Me.PictureBox5.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = Global.Arkadia.My.Resources.Resources.latihan
+        Me.PictureBox4.Location = New System.Drawing.Point(805, 502)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(161, 53)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.PictureBox4.TabIndex = 4
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = Global.Arkadia.My.Resources.Resources.pemadaman_listrik
+        Me.PictureBox3.Location = New System.Drawing.Point(805, 430)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(161, 53)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.PictureBox3.TabIndex = 3
+        Me.PictureBox3.TabStop = False
+        '
+        'AncamanBom
+        '
+        Me.AncamanBom.Image = Global.Arkadia.My.Resources.Resources.ancaman_boom
+        Me.AncamanBom.Location = New System.Drawing.Point(805, 357)
+        Me.AncamanBom.Name = "AncamanBom"
+        Me.AncamanBom.Size = New System.Drawing.Size(161, 53)
+        Me.AncamanBom.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.AncamanBom.TabIndex = 2
+        Me.AncamanBom.TabStop = False
+        '
+        'GempaBumi
+        '
+        Me.GempaBumi.Image = Global.Arkadia.My.Resources.Resources.gempa
+        Me.GempaBumi.Location = New System.Drawing.Point(805, 287)
+        Me.GempaBumi.Name = "GempaBumi"
+        Me.GempaBumi.Size = New System.Drawing.Size(161, 53)
+        Me.GempaBumi.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.GempaBumi.TabIndex = 1
+        Me.GempaBumi.TabStop = False
+        '
+        'Kebakaran
+        '
+        Me.Kebakaran.Image = Global.Arkadia.My.Resources.Resources.kebakaran
+        Me.Kebakaran.Location = New System.Drawing.Point(805, 216)
+        Me.Kebakaran.Name = "Kebakaran"
+        Me.Kebakaran.Size = New System.Drawing.Size(161, 53)
+        Me.Kebakaran.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.Kebakaran.TabIndex = 0
+        Me.Kebakaran.TabStop = False
+        '
+        'MenuUtama
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1016, 741)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "MenuUtama"
+        Me.Text = "MenuUtama"
+        Me.Panel1.ResumeLayout(False)
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.AncamanBom, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GempaBumi, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Kebakaran, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Kebakaran As PictureBox
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents AncamanBom As PictureBox
+    Friend WithEvents GempaBumi As PictureBox
+End Class
